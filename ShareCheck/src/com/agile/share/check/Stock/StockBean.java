2c0b7e3dc08f7e6823dbc96b821cbf30ce2f7efd
Blake 7 <kyle90adam@hotmail.com>
2012-11-14 06:52:51 +0000

    Commit on Wednesday the 14th at 06:52AM
    Complete version of Share Check API with unit tests for API only
    35 tests are now included in Test Class
    KNOWN ISSUES
    ---------------
    [Resolved] Missing Unit tests for User Story B
    Missing Unit Tests for User Story C

--- ShareCheck/src/com/agile/share/check/Stock/StockBean.java
+++ ShareCheck/src/com/agile/share/check/Stock/StockBean.java
@@ -19,27 +19,19 @@ public class StockBean {
     public void setPrice(float price) {
         this.price = price;
     }
-    public float getChange() {
-        return change;
-    }
+
     public void setChange(float change) {
         this.change = change;
     }
-    public String getChartUrlSmall() {
-        return chartUrlSmall;
-    }
+
     public void setChartUrlSmall(String chartUrlSmall) {
         this.chartUrlSmall = chartUrlSmall;
     }
-    public String getChartUrlLarge() {
-        return chartUrlLarge;
-    }
+
     public void setChartUrlLarge(String chartUrlLarge) {
         this.chartUrlLarge = chartUrlLarge;
     }
-    public long getLastUpdated() {
-        return lastUpdated;
-    }
+
     public void setLastUpdated(long lastUpdated) {
         this.lastUpdated = lastUpdated;
     }
